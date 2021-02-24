9256029434a0485cace7f99be9ae25bab492b4f7
huggggggggg <79512661+huggggggggg@users.noreply.github.com>
2021-02-25 02:01:19 +0600

    Add files via upload

---
+++ lab_2_v_9.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath> 
+
+using namespace std;
+int scan_int();
+double endless_row(int);
+
+int main()
+{
+	cout << "Enter the upper bound (integer): ";
+	int n = scan_int();
+	double result = endless_row(n);
+	cout << "The product is: " << result;
+}
+
+int scan_int(){ // Получение целого значения
+
+	while (true){
+
+		int input_data;
+		cin >> input_data;
+			if (cin.fail() || input_data < 0){ // Проверка на целое число с диапазоном от -32768 до 32767
+				cout << "Invalid value entered. Try again: \n";
+				cin.clear();
+				cin.ignore(32767, '\n');
+			}
+			else{
+				return input_data;
+			}
+		}
+}
+
+double endless_row(int n){
+
+	double production = 1;
+	for (int i = 1; i <= n; i++){
+		production = production * (1 + (pow((0.5), i*2)));
+	}
+	return production;
+}
