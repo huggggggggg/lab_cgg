85f3cc8ea8adc1fb4a2c70ffa32f5bbc93fb1389
huggggggggg <79512661+huggggggggg@users.noreply.github.com>
2021-03-10 17:26:19 +0600

    Update lab_2_v_9.cpp

--- lab_2_v_9.cpp
+++ lab_2_v_9.cpp
@@ -1,41 +1,43 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath> 
 using namespace std;
 
 int verify_value();
 double endless_row(int);
 
-
 int main()
 {
-	cout << "Enter the upper bound: "
-	int n = scan_int();
+	cout << "Enter the upper bound: ";
+	int n = verify_value();
 	double result = endless_row(n);
 	cout << "The product is: " << result;
 }
 
-int verify_value(){ 
-	
-	while (true){
-
+int verify_value()
+{
+	while (true)
+	{
 		int entered_value;
 		cin >> entered_value;
-		if (cin.fail() || entered_value < 0){
-		cout << "Invalid value entered. Try again: \n";
+		if (cin.fail() || entered_value < 0)
+		{
+			cout << "Invalid value entered. Try again: ";
 			cin.clear();
 			cin.ignore(32767, '\n');
 		}
-		else{
+		else
+		{
 			return entered_value;
 		}
 	}
 }
 
-double endless_row(int n){
-
+double endless_row(int n)
+{
 	double production = 1;
-	for (int i = 1; i <= n; i++){
-		production = production * (1 + (pow((0.5), i*2)));
+	for (int i = 0; i <= n; i++)
+	{
+		production = production * (1 + pow((0.5), i*2));
 	}
 	return production;
 }
